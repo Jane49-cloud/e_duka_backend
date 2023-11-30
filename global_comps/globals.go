9ba3912785e806756d728ba98fe25e5d7f2675be
eleli <lesosioayub@gmail.com>
2023-11-30 11:14:19 +0300

    removed vendor

--- global_comps/globals.go
+++ global_comps/globals.go
@@ -62,13 +62,12 @@ func ServeApplication() {
 
 	certFile := "./fullchain.pem"
 	keyFile := "./privkey.pem"
-
 	if err := router.RunTLS(":8000", certFile, keyFile); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-	// if err := router.Run("192.168.0.112:8000"); err != nil {
-	// 	log.Fatalf("Failed to start server: %v", err)
-	// }
+	//if err := router.Run("192.168.0.112:8000"); err != nil {
+	//	log.Fatalf("Failed to start server: %v", err)
+	//}
 
 	fmt.Println("Server running on port 8000 (HTTPS)")
 }
